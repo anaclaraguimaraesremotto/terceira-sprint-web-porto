63d2dceaa9e44a1b52ee161e57d1585841695c06
Ana Clara <anaclaraguimaraesremotto@gmail.com>
2023-09-19 16:00:33 -0300

    update

--- src/routes/Login/index.jsx
+++ src/routes/Login/index.jsx
@@ -11,7 +11,7 @@ export default function Login(){
                 <form action="/logar" name="login" method="post">
                         <div>
                             <label for="documento" className='identacao'>CPF/CNPJ:</label>
-                            <input type="number"  name="documento" required/>
+                            <input type="text"  name="documento" required/>
                         </div>
                         <div>
                             <label for="senha" className='identacao'>Senha:</label>
